163879f641907ea8c9cc57974001b9942259168c
Josh Stroschein <jstrosch@gmail.com>
2015-07-13 11:42:05 -0500

    adds css and js to be included as an asset

---
+++ web/js/application.js
@@ -0,0 +1,29 @@
+$(function() {
+
+    var dialog = $("#dialog-form").dialog({
+        autoOpen: false,
+        height: 300,
+        width: 350,
+        modal: true,
+        buttons: [
+        {
+            text: "Delete",
+            click: function() {
+
+                    $('#delete-kpa-form').submit();
+            }
+        },
+        {
+            text: "Cancel",
+            click: function() {
+                document.forms[0].reset();
+                $(this).dialog("close");
+            }
+        }
+    ]
+    });
+
+    $("#deletekpa").click(function() {
+        dialog.dialog("open");
+    });
+});
